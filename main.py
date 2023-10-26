5c0be375dba21395d9f66ba0c7825cfe7ca3e104
Igor Dobric <igordobricc@gmail.com>
2023-10-26 22:50:29 +0200

    test change

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__': # run web server only if file is ran directly
-    app.run(debug=True) # every time you update code web server reruns automatically (turn off in production)
+    app.run(debug=True) # every time you update code web server reruns automatically (turn off in production), this is a change
 
