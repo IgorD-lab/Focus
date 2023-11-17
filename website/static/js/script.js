b5620a0b865d69e2ce34fb86b4f09ed94f0a5cbd
Igor Dobric <igordobricc@gmail.com>
2023-11-17 18:49:09 +0100

    Full rework of navbar and added website logo

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -39,3 +39,23 @@ function deleteQuiz(quizId) {
     window.location.href = "/quiz"; // redirect to home page (refresh page)
   });
 }
+
+function toggle() {
+  var x = document.getElementById("dropdown-toggle");
+
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggleMobile() {
+  var x = document.getElementById("mobile-menu");
+
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
