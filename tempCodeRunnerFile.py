3bb2faf6ae46c6271f9a994c79b6179a1fd5a343
Igor D <igordobricc@gmail.com>
2024-05-11 03:54:54 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-if __name__ == '__main__': # run web server only if file is ran directly
-    app.run(debug=True) # every time you update code web server reruns automatically (turn off in production)
-
